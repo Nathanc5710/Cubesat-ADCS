c2fb64cf217b3d162c5dddb6e7683e7b4a27574a
Nathan Castonguay <nathanc5710@vt.edu>
2026-08-20 16:35:02 -0400

    Create the LQR controller and wire it into Plant to create the control loop

---
+++ matlab/design/lqr_design.m
@@ -0,0 +1,10 @@
+J = [1 0 0; 0 2 0; 0 0 3];  
+
+A = [zeros(3) eye(3); zeros(3) zeros(3)];
+B = [zeros(3); inv(J)];
+
+Q = diag([50 50 50 5 5 5]);   % penalize attitude error more than rate error
+R = diag([1 1 1]);            % moderate control effort penalty
+
+K = lqr(A,B,Q,R);
+disp(K)
